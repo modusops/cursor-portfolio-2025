d55f840e4f68d3a2461e4a937a1ae638f3f8bef6
Dave Chan <juntochan@gmail.com>
2025-06-01 20:39:15 -0700

    fix: update PlusSign component import path and add styled-jsx dependency

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link"
 import Image from "next/image"
-import { PlusSign } from "@/components/plus-sign"
+import { PlusSign } from "./components/plus-sign"
 import { useEffect, useState, useRef } from "react"
 import { AnimatedContent } from "@/components/animated-content"
 import styles from './styles/fade.module.css'
